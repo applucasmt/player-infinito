c4763dfdafbbf38dcd232d69208555c7a4c0dbe2
applucasmt <veogera1@gmail.com>
2026-09-19 19:02:23 -0400

    Add files via upload

---
+++ deepseek_javascript_20260919_f7bb71.js
@@ -0,0 +1,34 @@
+// Service Worker simples para funcionar offline
+const CACHE_NAME = 'player-infinito-v1';
+const URLS_TO_CACHE = [
+    './',
+    './index.html',
+    './manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(URLS_TO_CACHE))
+            .then(() => self.skipWaiting())
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys.filter(k => k !== CACHE_NAME)
+                    .map(k => caches.delete(k))
+            );
+        }).then(() => self.clients.claim())
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+            .catch(() => caches.match('./index.html'))
+    );
+});
